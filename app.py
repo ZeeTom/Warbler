d9ec9d59321dd16c61ff5613a8b3215ce9f239ee
ZeeTom <zthom13@hotmail.com>
2021-07-23 18:31:10 -0600

    deploying

--- app.py
+++ app.py
@@ -11,19 +11,23 @@ CURR_USER_KEY = "curr_user"
 DEFAULT_IMG = "/static/images/default-pic.png"
 DEFAULT_HEADER_IMG = "/static/images/warbler-hero.jpg"
 
-
+database_url = os.environ.get('DATABASE_URL', 'postgresql:///warbler')
+# fix incorrect database URIs currently returned by Heroku's pg setup
+database_url = database_url.replace('postgres://warbler', 'postgresql://warbler')
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
+# app.config['SQLALCHEMY_DATABASE_URI'] = (
+#     os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
-toolbar = DebugToolbarExtension(app)
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_ECHO'] = False
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+# toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 
